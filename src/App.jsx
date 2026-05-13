968619213e1be1a7d42323070b5e4c427acbebb2
sarangan16 <mailsarangan@gmail.com>
2026-05-13 20:54:35 +0200

    swaped order of why us - list of cars section

--- src/App.jsx
+++ src/App.jsx
@@ -10,8 +10,8 @@ export default function App() {
     <div className="min-h-screen">
       <Navbar />
       <Hero />
-      <CarList />
       <WhyChooseUs />
+      <CarList />
       <Testimonials />
       <Footer />
     </div>
